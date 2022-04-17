6fcf8658194f5f5066d096986e8e2b30c5998835
Rayyan-K <77406952+Rayyan-K@users.noreply.github.com>
2022-04-17 09:41:56 +0400

    sample data

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -8,6 +8,8 @@ import 'package:mushafmuscat/models/quarter.dart';
 import '../resources/dimens.dart';
 import '../widgets/surahs_list.dart';
 import '../widgets/quarters_list.dart';
+import '../widgets/sample_data.dart';
+
 
 class MainDrawer extends StatefulWidget {
   const MainDrawer({Key? key}) : super(key: key);
@@ -17,154 +19,10 @@ class MainDrawer extends StatefulWidget {
 }
 
 class _MainDrawerState extends State<MainDrawer> {
-  final List<Surah> _surah = [
-    Surah(
-      surahNum: "١",
-      surahTitle: "الفاتحة",
-      surahType: "مكية",
-      numOfAyas: "٧",
-    ),
-    Surah(
-      surahNum: "٢",
-      surahTitle: "البقرة",
-      surahType: "مكية",
-      numOfAyas: "٧",
-    ),
-    Surah(
-      surahNum: "٣",
-      surahTitle: "آل عمران",
-      surahType: "مكية",
-      numOfAyas: "٧",
-    ),
-    Surah(
-      surahNum: "٤",
-      surahTitle: "النساء",
-      surahType: "مكية",
-      numOfAyas: "٧",
-    ),
-    Surah(
-      surahNum: "٥",
-      surahTitle: "المائدة",
-      surahType: "مكية",
-      numOfAyas: "٧",
-    ),
-    Surah(
-      surahNum: "٦",
-      surahTitle: "الأنعام",
-      surahType: "مكية",
-      numOfAyas: "٧",
-    ),
-    Surah(
-      surahNum: "٧",
-      surahTitle: "النساء",
-      surahType: "مكية",
-      numOfAyas: "٧",
-    ),
-    Surah(
-      surahNum: "٨",
-      surahTitle: "المائدة",
-      surahType: "مكية",
-      numOfAyas: "٧",
-    ),
-  ];
+  
+  final List<Surah> _surah = surah;
+    final List<Quarter> _quarter = quarter;
 
-  final List<Quarter> _quarter = [
-    Quarter(
-      startingJuzzIndex: true,
-      startingHizbIndex: true,
-      quarter: 1,
-      hizbNum: "١",
-      surahTitle: "الفاتحة",
-      startingAya: "الْحَمْدُ لِلَّهِ رَبِّ الْعَالَمِينَ ",
-      juzz: "الجزء الأول",
-      quarterAyaNum: "١",
-      quarterPageNum: "١",
-    ),
-    Quarter(
-      startingJuzzIndex: false,
-      startingHizbIndex: false,
-      quarter: 2,
-      hizbNum: "١",
-      surahTitle: "البقرة",
-      startingAya: "إِنَّ اللَه لَا يَسْتَحْيِي أَن يَضْرِبَ مَثَلًا مَّا...",
-      juzz: "الجزء الأول",
-      quarterAyaNum: "٢٦",
-      quarterPageNum: "٥",
-    ),
-    Quarter(
-      startingJuzzIndex: false,
-      startingHizbIndex: false,
-      quarter: 3,
-      hizbNum: "١",
-      surahTitle: "البقرة",
-      startingAya:
-          "أَتَأْمُرُونَ النَّاسَ بِالْبِرِّ وَتَنسَوْنَ أَنفُسَكُمْ ...",
-      juzz: "الجزء الأول",
-      quarterAyaNum: "٤٤",
-      quarterPageNum: "٧",
-    ),
-    Quarter(
-      startingJuzzIndex: false,
-      startingHizbIndex: false,
-      quarter: 4,
-      hizbNum: "١",
-      surahTitle: "البقرة",
-      startingAya: "وَإِذِ اسْتَسْقَىٰ مُوسَىٰ لِقَوْمِهِ فَقُلْنَا اضْرِب...",
-      juzz: "الجزء الأول",
-      quarterAyaNum: "٦٠",
-      quarterPageNum: "٩",
-    ),
-    // new hizb
-    Quarter(
-      startingJuzzIndex: false,
-      startingHizbIndex: true,
-      quarter: 1,
-      hizbNum: "٢",
-      surahTitle: "البقرة",
-      startingAya:
-          "أَفَتَطْمَعُونَ أَن يُؤْمِنُوا لَكُمْ وَقَدْ كَانَ فَرِيقٌ...",
-      juzz: "الجزء الأول",
-      quarterAyaNum: "٧٥",
-      quarterPageNum: "١١",
-    ),
-    Quarter(
-      startingJuzzIndex: false,
-      startingHizbIndex: false,
-      quarter: 2,
-      hizbNum: "٢",
-      surahTitle: "البقرة",
-      startingAya:
-          "وَلَقَدْ جَآءَكُم مُّوسَىٰ بِٱلْبَيِّنَٰتِ ثُمَّ ٱتَّخَذْتُمُ...",
-      juzz: "الجزء الأول",
-      quarterAyaNum: "٩٢",
-      quarterPageNum: "١٤",
-    ),
-    Quarter(
-      startingJuzzIndex: false,
-      startingHizbIndex: false,
-      quarter: 3,
-      hizbNum: "٢",
-      surahTitle: "البقرة",
-      startingAya:
-          "مَا نَنْسَخْ مِنْ آيَةٍ أَوْ نُنْسِهَا نَأْتِ بِخَيْرٍ مِنْهَا...",
-      juzz: "الجزء الأول",
-      quarterAyaNum: "١٠٦",
-      quarterPageNum: "١٧",
-    ),
-    Quarter(
-      startingJuzzIndex: false,
-      startingHizbIndex: false,
-      quarter: 4,
-      hizbNum: "٢",
-      surahTitle: "البقرة",
-      startingAya:
-          "وَإِذِ ابْتَلَىٰ إِبْرَاهِيمَ رَبُّهُ بِكَلِمَاتٍ فَأَتَمَّهُنَّ ...",
-      juzz: "الجزء الأول",
-      quarterAyaNum: "١٢٤",
-      quarterPageNum: "١٩",
-    ),
-    // new juzz
-  ];
 
   int segmentedControlValue = 0;
   String hint = 'البحث عن سورة';
