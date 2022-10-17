2fe5f83bebaa92710c1fec63389788e5c59551dc
Rayyan-K <77406952+Rayyan-K@users.noreply.github.com>
2022-10-17 12:06:37 +0400

    checking network for daily aya api

--- lib/providers/dailyAya_provider.dart
+++ lib/providers/dailyAya_provider.dart
@@ -6,14 +6,23 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+Future<bool> hasNetwork() async {
+  try {
+    final result = await InternetAddress.lookup('api.omanfont.om');
+    return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
+  } on SocketException catch (_) {
+    return false;
+  }
+}
 
 Future<dailyAya> getSinglePostData(context) async {
   late dailyAya result;
+
   try {
-    final response = await http
-        .get(Uri.parse('https://api.omanfont.om/'),headers: {
-        HttpHeaders.contentTypeHeader: "application/json",
-      });
+    final response =
+        await http.get(Uri.parse('https://api.omanfont.om/'), headers: {
+      HttpHeaders.contentTypeHeader: "application/json",
+    });
     if (response.statusCode == 200) {
       final item = json.decode(response.body);
       result = dailyAya.fromJson(item);
@@ -32,7 +41,11 @@ class dailyAya {
   final String Surah;
   final String Tafsir;
 
-  dailyAya({required this.id, required this.Aya, required this.Surah, required this.Tafsir});
+  dailyAya(
+      {required this.id,
+      required this.Aya,
+      required this.Surah,
+      required this.Tafsir});
 
   factory dailyAya.fromJson(Map<String, dynamic> json) {
     return dailyAya(
@@ -49,12 +62,12 @@ class dailyAyaProvider with ChangeNotifier {
   bool loading = false;
 
   getPostData(context) async {
-    loading = true;
-    post = await getSinglePostData(context);
-    loading = false;
-print(post.Aya);
+    final bool checkInternet = await hasNetwork();
+    print("checking internet $checkInternet");
+    if (checkInternet == true) {
+      post = await getSinglePostData(context);
+    }
+    print(post.Aya);
     notifyListeners();
   }
 }
-
-  
