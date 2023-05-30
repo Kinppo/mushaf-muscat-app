8bb6853c9b6ad54dacdcb2a1f65372918b059fb2
Rayyan-K <77406952+Rayyan-K@users.noreply.github.com>
2023-05-30 22:02:54 +0400

    Change the scaffold background color

--- lib/themes/app_themes.dart
+++ lib/themes/app_themes.dart
@@ -4,7 +4,7 @@ import 'package:hexcolor/hexcolor.dart';
 class AppThemes {
   static final ThemeData lightTheme = ThemeData(
       primaryColor: HexColor('#e3d8cc'),
-      scaffoldBackgroundColor: HexColor('#faf6f2'),
+      scaffoldBackgroundColor: HexColor('#f5efe9'),
       accentColor: HexColor('#c7beb5'),
       buttonColor: HexColor('#ffffff'),
       hintColor: HexColor('#db0f00'),
