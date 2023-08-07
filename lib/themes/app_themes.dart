b22dfea72b498fe63f648ea426ade221b6b7e015
Rayyan-K <77406952+Rayyan-K@users.noreply.github.com>
2023-08-08 00:38:27 +0400

    Edit app theme

--- lib/themes/app_themes.dart
+++ lib/themes/app_themes.dart
@@ -21,7 +21,7 @@ class AppThemes {
             fontFamily: "IBMPlexSansArabic",
             fontWeight: FontWeight.w200,
             ),
-        headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+        headline6: TextStyle(fontSize: 36.0, fontFamily: 'IBMPlexSansArabic'),
         bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'IBMPlexSansArabic'),
         labelMedium: TextStyle(fontSize: 16.0, fontFamily: 'IBMPlexSansArabic', color: Colors.black,fontWeight: FontWeight.bold),
       ),);
